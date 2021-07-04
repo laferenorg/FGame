1e34f33c39c92a39ac36b26cae59d35189833b12
Jonathan Marpaung <60574415+jonathanmarp@users.noreply.github.com>
2021-07-04 21:39:33 +0700

    Adding testing and LICENSE file
    I'am adding testing file, In this time i just using os linux, I never using windows in this version project.
    I using compiler clang version 10.

---
+++ test/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <FGame.hpp>
+
+/* Create variable global sprite image */
+FG_Image player1;
+FG_Rect rectPlayer;
+
+int main(int argc, char const *argv[])
+{
+	/* Setup */
+	FGameV2 game({ FG_INIT, FG_IMAGE }, "FGame");
+	game.SetIcon("assets/icon.png");
+	game.SetSize(400, 200);
+	game.SetPosition();
+
+	/* Setup Player */
+	game.Run.Image.Load(player1, "assets/icon.png", FG_THIS_DEFAULT(game));
+	rectPlayer.x = 0;
+	rectPlayer.y = 0;
+	rectPlayer.width = player1.width;
+	rectPlayer.height = player1.height;
+	game.Run.Set.SetRectSize(rectPlayer, rectPlayer.width * 3, rectPlayer.height * 3);
+
+	/* Start looping */
+	game.StartLooping(
+	/* Handle Event function */
+	[](SDL_Event& event, FGameV2* back) {
+		/* Set background color */
+		back->SetBackground({ 0, 255, 0 });
+	},
+	/* Update function */
+	[](FGameV2* back) {
+		back->Run.Set.SetRectPosition(rectPlayer, rectPlayer.x + 2, rectPlayer.y);
+		back->Run.Draw.Rect({ 255, 255, 255 }, rectPlayer, false, FG_THIS_POINTER(back));
+		back->Run.Image.Render(player1, rectPlayer, false, FG_THIS_POINTER(back));
+	},
+	/* Settings fps */
+	60);
+	{
+		(void)argc;
+		(void)argv;
+	}
+	return 0;
+}
