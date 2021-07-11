021bdb6efe19dd830104df25a6c9d27c0f9802a9
Jonathan Marpaung <60574415+jonathanmarp@users.noreply.github.com>
2021-07-11 21:47:03 +0700

    :memo: Add some features save and load file
    I adding features for saving and load file

--- example/DrawPixel.cpp
+++ example/DrawPixel.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <ios>
+#include <fstream>
 
 /* Include library FGame */
 #include <FGame/FGame.hpp>
@@ -28,11 +30,79 @@ std::vector<Box> box_tools_array;
 std::vector<FG_Color> color_array;
 FG_Rect mouseRect;
 int id = 0;
+std::string filePATH = "";
+bool ExistFile = false;
 
 /* Handle events */
 void handleEvents(SDL_Event& event, FGameV2* fgame) {
 	/* Set background */
 	fgame->SetBackground({ 241, 241, 241, 1 });
+
+	/* Handle event key */
+	switch(event.type) {
+		case SDL_QUIT: {
+			/* Check name file isn't empty */
+			if(!filePATH.empty()) {
+				/* If variable name file isn't empty */
+				/* Setup variable file management */
+				std::ofstream fileManage;
+
+				/* Open file */
+				fileManage.open(filePATH, std::ios::trunc | std::ios::binary);
+				
+				/* Write box */
+				for(auto& box : box_array) {
+					fileManage.write((char*)&box, sizeof(Box));
+				}
+
+				/* Close file */
+				fileManage.close();
+			}
+		}
+		break;
+		case SDL_KEYDOWN: {
+			switch(event.key.keysym.sym) {
+				case SDLK_1: {
+					fgame->Run.Set.SetRectSize(fgame->System.cursor.rect, 10, 10);
+				}
+				break;
+				case SDLK_2: {
+					fgame->Run.Set.SetRectSize(fgame->System.cursor.rect, 20, 20);
+				}
+				break;
+				case SDLK_3: {
+					fgame->Run.Set.SetRectSize(fgame->System.cursor.rect, 30, 30);
+				}
+				break;
+				case SDLK_4: {
+					fgame->Run.Set.SetRectSize(fgame->System.cursor.rect, 40, 40);
+				}
+				break;
+				case SDLK_5: {
+					fgame->Run.Set.SetRectSize(fgame->System.cursor.rect, 50, 50);
+				}
+				break;
+				case SDLK_6: {
+					fgame->Run.Set.SetRectSize(fgame->System.cursor.rect, 60, 60);
+				}
+				break;
+				case SDLK_7: {
+					fgame->Run.Set.SetRectSize(fgame->System.cursor.rect, 70, 70);
+				}
+				break;
+				case SDLK_8: {
+					fgame->Run.Set.SetRectSize(fgame->System.cursor.rect, 80, 80);
+				}
+				break;
+				case SDLK_9: {
+					fgame->Run.Set.SetRectSize(fgame->System.cursor.rect, 90, 90);
+				}
+				break;
+			}
+			break;
+		}
+		break;
+	}
 
 	/* If cursor click box */
 	for(auto& box : box_array) {
@@ -78,29 +148,81 @@ void update(FGameV2* fgame) {
 }
 
 int main(int argc, const char* argv[]) {
+	/* Get location file for save */
+	std::cout << "Input File: ";
+	std::getline(std::cin, filePATH);
+	if(!filePATH.empty()) ExistFile = true;
+	
+	/* Test exist file */
+	if(ExistFile) {
+		/* Setup variable file management */
+		std::ifstream testFile;
+
+		/* Test open file */
+		testFile.open(filePATH, std::ios::binary);
+		
+		/* Check if file can't open */
+		if(!testFile.is_open()) ExistFile = false;
+		
+		/* Close file */
+		testFile.close();
+	}
+
 	/* Setup */
 	FGameV2 Manager = FGameV2({ FG_INIT }, "Draw Pixel");
 	
 	/* Settings Window */
 	Manager.SetSize(WIDTH, HEIGHT);
+	Manager.Run.Set.SetRectSize(Manager.System.cursor.rect, 10, 10);
 	Manager.Run.Cursor.Show(false, FG_THIS_DEFAULT(Manager));
-
+	#if !defined(_WIN32)
+		Manager.SetIcon("/home/jonathan/.face");
+	#endif
+		
 	/* Load row and colum */
-	for(int ROW = 0; ROW < ROWS; ROW++) {
-		for(int COL = 0; COL < COLS; COL++) {
+	if(!ExistFile) {
+		/* If not exist file */
+		for(int ROW = 0; ROW < ROWS; ROW++) {
+			for(int COL = 0; COL < COLS; COL++) {
+				/* Setup box */
+				Box tempBox;
+				Manager.Run.Set.SetRectSize(tempBox.rect, TILE_SIZE - 2, TILE_SIZE - 2);
+				Manager.Run.Set.SetRectSize(tempBox.underBox, TILE_SIZE, TILE_SIZE);
+
+				Manager.Run.Set.SetRectPosition(tempBox.rect, 
+					TILE_SIZE * COL - 2, TILE_SIZE * ROW - 2);
+				Manager.Run.Set.SetRectPosition(tempBox.underBox, 
+					TILE_SIZE * COL, TILE_SIZE * ROW);
+
+				/* Push box */
+				box_array.push_back(tempBox);
+			}
+		}
+	}
+
+	if(ExistFile) {
+		/* If file exist */
+		/* Setup variable load pixel */
+		std::ifstream LoadPixel;
+		
+		/* Open file */
+		LoadPixel.open(filePATH, std::ios::binary);
+
+		/* Load */
+		for(int i = 0; i < (ROWS * COLS); i++) {
 			/* Setup box */
 			Box tempBox;
-			Manager.Run.Set.SetRectSize(tempBox.rect, TILE_SIZE - 2, TILE_SIZE - 2);
-			Manager.Run.Set.SetRectSize(tempBox.underBox, TILE_SIZE, TILE_SIZE);
 
-			Manager.Run.Set.SetRectPosition(tempBox.rect, 
-				TILE_SIZE * COL - 2, TILE_SIZE * ROW - 2);
-			Manager.Run.Set.SetRectPosition(tempBox.underBox, 
-				TILE_SIZE * COL, TILE_SIZE * ROW);
+			/* Read */
+			LoadPixel.seekg(sizeof(Box) * i);
+			LoadPixel.read((char*)&tempBox, sizeof(Box));
 
-			/* Push box */
+			/* Push back */
 			box_array.push_back(tempBox);
 		}
+
+		/* Close file */
+		LoadPixel.close();
 	}
 	
 	/* Load color */
